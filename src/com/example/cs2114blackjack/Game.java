9354811e8e3349ad210872ff116afcc8e8ca15e0
jordandeang <jdeang@vt.edu>
2015-04-07 18:49:00 -0400

    Game method stubs

---
+++ src/com/example/cs2114blackjack/Game.java
@@ -0,0 +1,34 @@
+package com.example.cs2114blackjack;
+
+import java.util.Queue;
+import java.util.ArrayList;
+
+public class Game
+{
+
+    //fields
+    private ArrayList<Card> discard;
+    private Queue<Card> deck;
+
+    //initialize
+    public Game()
+    {
+        fillDiscard();
+        shuffleDeck();
+
+    }
+
+    private void shuffleDeck()
+    {
+        // TODO take the cards from the discard into the deck
+
+    }
+
+    private void fillDiscard()
+    {
+        // TODO fill the discard pile with all of the cards necessary
+
+    }
+
+
+}
