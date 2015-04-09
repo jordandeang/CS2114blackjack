051598af7f475d38ad81f03191a18c647c969fe0
jordandeang <jdeang@vt.edu>
2015-04-09 10:36:13 -0400

    Add player class and discard()

--- src/com/example/cs2114blackjack/Game.java
+++ src/com/example/cs2114blackjack/Game.java
@@ -1,5 +1,6 @@
 package com.example.cs2114blackjack;
 
+import java.util.EmptyStackException;
 import sofia.util.Random;
 import java.util.Stack;
 import java.util.ArrayList;
@@ -8,28 +9,37 @@ public class Game
 {
 
     // fields
-    private ArrayList<Card> discard;
-    private Stack<Card>     deck;
-    private ArrayList<Card> playerHand;
-    private ArrayList<Card> dealerHand;
+    private ArrayList<Card>  discard;
+    public Stack<Card>       deck;
+    private Player           player;
+    private Player           dealer;
+    private static final int NUMBER_OF_DECKS = 1;
 
 
     // initialize
+    // ----------------------------------------------------------
+    /**
+     * Create a new Game object.
+     */
     public Game()
     {
         discard = new ArrayList<Card>();
         deck = new Stack<Card>();
-        playerHand = new ArrayList<Card>();
-        dealerHand = new ArrayList<Card>();
-        fillDiscard(1);
+        fillDiscard();
         shuffleDeck();
+        player = new Player();
+        dealer = new Player();
+        dealHands();
     }
 
 
     // methods
-    public void shuffleDeck()
+    // ----------------------------------------------------------
+    /**
+     * Shuffles the deck with cards from the discard pile
+     */
+    private void shuffleDeck()
     {
-        // TODO take the cards from the discard into the deck
         while (!discard.isEmpty())
         {
             deck.push(discard.remove(Random.generator().nextInt(
@@ -40,13 +50,11 @@ public class Game
 
 
     /**
-     * @param n
-     *            the number of 52 card decks used.
+     * Fills the discard pile with the given amount of decks.
      */
-    public void fillDiscard(int n)
+    private void fillDiscard()
     {
-        // TODO fill the discard pile with all of the cards necessary
-        for (int d = 0; d < n; d++)
+        for (int d = 0; d < NUMBER_OF_DECKS; d++)
         {
             for (int i = 1; i < 14; i++)
             {
@@ -59,23 +67,78 @@ public class Game
         }
     }
 
+
     /**
      * Deals cards to the player and dealer
      */
     public void dealHands()
     {
-        playerHand.add(deck.pop());
-        dealerHand.add(deck.pop());
-        playerHand.add(deck.pop());
-        dealerHand.add(deck.pop());
+        hit(player);
+        hit(dealer);
+        hit(player);
+        hit(dealer);
     }
 
+
+    // ----------------------------------------------------------
     /**
-     * player chooses to hit
+     * Discards hands into the discard pile
      */
-    public void hit()
+    public void discardHands()
     {
-        playerHand.add(deck.pop());
+        for (int i = 0; i < player.getHand().size(); i++)
+        {
+            Card newCard = player.getHand().remove(i);
+            newCard.flipCardDown();
+            discard.add(newCard);
+        }
+        for (int i = 0; i < dealer.getHand().size(); i++)
+        {
+            Card newCard = dealer.getHand().remove(i);
+            newCard.flipCardDown();
+            discard.add(newCard);
+        }
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Hit the given player with a card, shuffle if the deck is empty
+     *
+     * @param p
+     *            the player to be hit
+     */
+    public void hit(Player p)
+    {
+        if (deck.size() == 0)
+        {
+            shuffleDeck();
+        }
+        Card card = deck.pop();
+        card.flipCardUp();
+        p.addCard(card);
+        if (p.getScore() > 21)
+        {
+            // TODO Bust, dealer wins, discard hands and redeal
+        }
+    }
+
+
+    public void stand()
+    {
+        // TODO figure out stand
+    }
+
+
+    public Player getDealer()
+    {
+        return dealer;
+    }
+
+
+    public Player getPlayer()
+    {
+        return player;
     }
 
 }
