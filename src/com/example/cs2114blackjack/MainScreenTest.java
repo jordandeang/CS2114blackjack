54469b659808c38d3457b1ae36edcfa8dfc04f91
Tommy Hyres <tom4vt@gmail.com>
2015-04-27 13:20:40 -0400

    added test classes

--- src/com/example/cs2114blackjack/MainScreenTest.java
+++ src/com/example/cs2114blackjack/MainScreenTest.java
@@ -1,6 +1,19 @@
 package com.example.cs2114blackjack;
 
-public class MainScreenTest
+import student.TestCase;
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author1 Jason Davis (jdavis7)
+ *  @author2 Jordan Deang (jdeang)
+ *  @author3 Tommy Hyres (tommy085)
+ *  @version 2015.04.27
+ */
+public class MainScreenTest extends TestCase
 {
 
 }
